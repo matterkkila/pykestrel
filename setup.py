831c1f51df0b9d2419d49946f417cec9143e33c3
Matt Erkkila <matt@matterkkila.com>
2010-12-03 14:44:48 -0800

    setup.py updates for pypi.

--- setup.py
+++ setup.py
@@ -3,18 +3,27 @@ from setuptools import setup, find_packages
 
 setup(
     name='pykestrel',
-    version='0.0.1',
+    version='0.0.2',
     description='A python kestrel client',
-    long_description='A python kestrel client',
+    long_description='A python kestrel client based on the python-memcached library.',
     keywords='queues kestrel memcached',
     author='Matt Erkkila',
     author_email='matt@matterkkila.com',
-    url='http://github.com/empower/pykestrel',
+    maintainer='Matt Erkkila',
+    maintainer_email='matt@matterkkila.com',
+    url='https://github.com/matterkkila/pykestrel',
+    download_url='https://github.com/matterkkila/pykestrel/tarball/0.0.2',
     license='BSD',
     packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
     include_package_data=True,
     zip_safe=True,
     install_requires=[
         'python-memcached>=1.45',
-    ]
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python',
+    ],
 )
